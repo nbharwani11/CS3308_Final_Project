11ea61d46a23618b0705f6a1b68228b6d8d3bb4d
Naureen Bharwani <naureen.bharwani@colorado.edu>
2021-07-08 21:51:30 -0500

    initial Homepage commits

--- pokemon/src/components/Home.js
+++ pokemon/src/components/Home.js
@@ -3,7 +3,29 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <Link to={`/pokedex`} className="alert alert-primary">POKEDEX</Link>
+        <div>
+            <div className="home">
+                <center><img src="pokemon2.png" width="300" height="200"/></center>
+            </div>
+
+            <div className="home" style={{"text-align": "center"}} >
+                <h1> Welcome Pokémon Trainers!</h1>
+            </div>
+            
+            <center><p>
+                Welcome to the world of Pokémon! 
+                This world is inhabited by creatures called Pokémon! 
+                For some people, Pokémon are pets. Others use them for fights. 
+                Your very own Pokémon adventure is about to unfold! 
+                A world of dreams and adventures with Pokémon awaits!
+                Let's go!
+            </p></center>
+
+            <center><img src="pokemon5.jpeg" width="300" height="200"/></center>
+
+            <Link to={`/pokedex`} className="alert alert-primary">POKEDEX</Link>
+
+        </div>
     )
 };
 

23bb59ab7273ef7d05485dd649db04c4b4f6be72
Naureen Bharwani <naureen.bharwani@colorado.edu>
2021-07-12 23:32:57 -0500

    updates to App.test.js

--- pokemon/src/App.test.js
+++ pokemon/src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React, {Provider} from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen, shallow } from '@testing-library/react';
+import Home from './components/Home';
+import Bag from './components/Bag';
 import App from './App';
+import PokemonDetails from './components/PokemonDetails';
+import PokemonList from './components/PokemonList';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Home', () => {
+    render(<BrowserRouter > <Home /> </BrowserRouter>);
+    const linkTitle = screen.getByText(/Others/i);
+    expect(linkTitle).toBeInTheDocument();
 });
+
+test('renders Bag', () => {
+    const bagData = [{name:'bulbasaur', sprites: {front_default:''}}];
+    render(<BrowserRouter > <Bag bag = {bagData} /> </BrowserRouter>);
+    const linkTitle = screen.getByText(/bulbasaur/i);
+    expect(linkTitle).toBeInTheDocument();
+});
+
+test('renders PokemonList', () => {
+    const pokemonData = [{name:'bulbasaur', sprites: {front_default:''}}, {name:'charizard', sprites: {front_default:''}}];
+    const filterString = 'bulb';
+    const bagData = [];
+    render(<BrowserRouter > <PokemonList pokemons = {pokemonData} bag = {bagData} filter = {filterString} /> </BrowserRouter>);
+    const linkTitle = screen.getByText(/bulb/i);
+    expect(linkTitle).toBeInTheDocument();
+});
+

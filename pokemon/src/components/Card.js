15cf05456f26dcb6bd337c02038594fcbb2279d4
Naureen Bharwani <naureen.bharwani@colorado.edu>
2021-08-03 19:29:41 -0500

    updates to styling card and pokedex

--- pokemon/src/components/Card.js
+++ pokemon/src/components/Card.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import "./PokemonDetails.css"
-import "./Pokedex.css"
-
 const Card = ({ pokemon, showDetails, bag, isBag, updateBag }) => {
     const changeHandler = () => {
         updateBag(pokemon)
     }
 
     return (
-<<<<<<< HEAD
-        <div className="card">
-            <div className={isBag ? "col-sm-12 col-md-2" : null} style={{width: "20%"}}>
-                <div className="card text-center">
-                    <div className="card -header">{ pokemon.name }</div>
-                    <div className="card -content">
-                        <img src={ pokemon.sprites.front_default } className="card-img-top" alt={ pokemon.name } />
-                    </div>
-                    <div className="card -footer">
-                        {showDetails && <Link to={`/pokedex/${pokemon.id}`} className="btn btn-primary">Details</Link>}
-                        {bag.includes(pokemon) ? (
-                            <button className="btn btn-warning" onClick={() => changeHandler()}>
-                                Remove
-                            </button> ) : (
-                            <button className="btn btn-success" onClick={() => changeHandler()}>
-                                Add
-                            </button>)
-                        }    
-                    </div>
-                </div>
-=======
         <div className="card text-center">
             <div className="card -header">{ pokemon.name }</div>
             <div className="card -content">
@@ -46,7 +22,6 @@ const Card = ({ pokemon, showDetails, bag, isBag, updateBag }) => {
                         Add
                     </button>)
                 }    
->>>>>>> 7cf75ebbec5c936a23719a3b8a4bceecb1ae9f69
             </div>
         </div>
     )
--- pokemon/src/components/Pokedex.js
+++ pokemon/src/components/Pokedex.js
@@ -10,10 +10,8 @@ import SearchBar from './SearchBar';
 import Sound from 'react-sound';
 import palettetown from '../utils/audio/palettetown_pokedex.mp3';
 
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
-import { Container, Modal } from 'react-bootstrap';
-
-import "./Pokedex.css"
 
 class Pokedex extends Component {
     state = {
@@ -39,80 +37,6 @@ class Pokedex extends Component {
         console.log("bag", bag)
         console.log("move", pokemons.name)
         return (
-<<<<<<< HEAD
-            <div className="pokedex">
-                <div className="p-3 mb-2 bg-light text-dark">
-                <div className="container">
-                    <div className="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" className="btn btn-outline-primary"><Link to={"../"}>HOME</Link></button>
-                        <button type="button" className="btn btn-outline-primary"><Link to={"/battle"}>BATTLE</Link></button>
-                    </div>
-                    <Bag 
-                        bag={bag}
-                        updateBag={updateBag}
-                    />
-                    <SearchBar 
-                        setFilterPokemon={this.filterPokemon}
-                    />
-
-                    <select name="types" value={this.state.typeFilter} onChange={(event) => this.setState({ typeFilter:event.target.value })}>
-                        <option value="">sort pokemon by primary type</option>
-                        {pokemonType.map(type => <option key={ type } value={type.toLowerCase()}>{type}</option>)}
-                    </select>  
-
-                    <label htmlFor = "points">
-                        Base Value
-                    </label>  
-                    <input
-                        type="range" 
-                        id="points" 
-                        name="points" 
-                        min="1" 
-                        max="300" 
-                        value = {this.state.maxBaseExperienceFilter} 
-                        onChange={(event) =>  this.setState({maxBaseExperienceFilter:event.target.value })}
-                    />
-
-                    //put grid system here
-                    <div>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                                <div class="col-sm">
-                                  One of three columns
-                                </div>
-                                <div class="col-sm">
-                                  One of three columns
-                                </div>
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                                <div class="w-100"></div>
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                                <div class="col-sm">
-                                One of three columns
-                                </div>
-                            </div>
-                        </div>
-                    </div>    
-
-=======
             <div className="p-3 mb-2 bg-light text-dark">
             <div className="container">
                 <div className="btn-group" role="group" aria-label="Basic example">
@@ -146,7 +70,6 @@ class Pokedex extends Component {
                 />                
                 
                 <div class="row">
->>>>>>> 7cf75ebbec5c936a23719a3b8a4bceecb1ae9f69
                     <PokemonList 
                         pokemons={pokemons}
                         nameFilter={this.state.nameFilter}
@@ -155,18 +78,6 @@ class Pokedex extends Component {
                         bag={bag}
                         updateBag={updateBag}
                     />
-<<<<<<< HEAD
-
-
-                </div>
-                <Sound 
-                    url={palettetown}
-                    playStatus={Sound.status.PLAYING}
-                    playFromPosition={300}
-                    loop={true}
-                />            
-                </div>
-=======
                 </div>
                         
                         
@@ -177,7 +88,6 @@ class Pokedex extends Component {
                 playFromPosition={300}
                 loop={true}
             />            
->>>>>>> 7cf75ebbec5c936a23719a3b8a4bceecb1ae9f69
             </div>
         )
     }
